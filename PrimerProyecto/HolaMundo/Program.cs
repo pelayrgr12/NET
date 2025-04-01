dc37e951c5e707a3eda77ce9b57cbac92418bc69
Pelayo González Rodríguez <122565894+pelayrgr12@users.noreply.github.com>
2025-04-01 10:02:02 +0200

    commit

--- PrimerProyecto/HolaMundo/Program.cs
+++ PrimerProyecto/HolaMundo/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Persona persona1 = new Persona("Juan", 30);
+persona1.Saludar();
+Console.WriteLine(persona1.ToString());
